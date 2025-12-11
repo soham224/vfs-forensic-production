7aab08207692928a338a53e57a719004b5487505
soham224 <ATBBha2k8Cek89mNpjdNJSZsbPVd9DE540D2>
2025-12-11 18:48:15 +0530

    latest code push for docker  and all

--- frontend/src/utils/ipConfig.js
+++ frontend/src/utils/ipConfig.js
@@ -1,4 +1,4 @@
 
-export  const  REACT_APP_IP_Config = "http://" +  window.location.hostname + ":8000" + + "/docs"
+export  const  REACT_APP_IP_Config = "http://" +  window.location.hostname + ":9000" + + "/docs"
 
-export  const  REACT_APP_API_HOST_IP_Config = "http://" + window.location.hostname + ":8000" + '/api/v1'
+export  const  REACT_APP_API_HOST_IP_Config = "http://" + window.location.hostname + ":9000" + '/api/v1'
